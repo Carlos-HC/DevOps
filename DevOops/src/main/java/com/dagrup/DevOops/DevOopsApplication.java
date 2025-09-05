60157031eb7e63f05f8dea4da1195d30d52c6101
unknown <carlosi.huerta@udem.edu>
2025-09-05 10:37:57 +0200

    Unzipping

---
+++ DevOops/src/main/java/com/dagrup/DevOops/DevOopsApplication.java
@@ -0,0 +1,13 @@
+package com.dagrup.DevOops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOopsApplication.class, args);
+	}
+
+}
---
+++ DevOops/src/test/java/com/dagrup/DevOops/DevOopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dagrup.DevOops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
